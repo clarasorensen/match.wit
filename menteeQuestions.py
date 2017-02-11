4d8c0b33c85a6390e8787c008b157968b5e1ed89
clarasorensen <csorense@wellesley.edu>
2017-02-11 12:10:05 -0500

    adding mentee questions

---
+++ menteeQuestions.py
@@ -0,0 +1,12 @@
+#match.wit
+#mentee questions
+
+
+name
+communication = ['email', 'phone', 'Skype', 'text', 'in-person'] #preference--select all that apply
+age = ['10-15', '16-20', '21-25', '26-30', '30+']
+location #city, state, country*
+gender = [0, 1] #0 = woman, 1 = man
+spokenLanguages = ['English', 'Chinese', 'Spanish', 'Arabic', 'Portuguese', 'Japanese', 'Malaysian', 'Russian', 'French', 'German']
+codingLanguages = ['Python', 'JavaScript', 'Java', 'PHP', 'Ruby', 'SQL', 'C#', 'C++', 'iOS', 'other'] #select all that apply
+
