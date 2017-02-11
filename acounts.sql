32e8d882f5c221b304fb0f0a747a85bb07c5a1d4
clarasorensen <csorense@wellesley.edu>
2017-02-11 18:11:08 -0500

    just gonna push anyway

---
+++ acounts.sql
@@ -0,0 +1,15 @@
+-- Accounts table 
+
+use whack17;
+
+drop table if exists mentor_survey;
+drop table if exists mentee_survey;
+drop table if exists match;
+drop table if exists accounts;
+
+create table accounts (
+	email varchar(100),
+	password varchar(50),
+	type enum('mentor','mentee', 'admin'),
+	INDEX(email)
+) ENGINE = INNODB;
