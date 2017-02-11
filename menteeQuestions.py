1bc53c8731c6410bbf70d718ae1d3c5d5a527c55
clarasorensen <csorense@wellesley.edu>
2017-02-11 12:13:59 -0500

    work plz

--- menteeQuestions.py
+++ menteeQuestions.py
@@ -1,12 +1,14 @@
 #match.wit
 #mentee questions
 
-
+#both mentor/mentee questions
 name
 communication = ['email', 'phone', 'Skype', 'text', 'in-person'] #preference--select all that apply
 age = ['10-15', '16-20', '21-25', '26-30', '30+']
 location #city, state, country*
-gender = [0, 1] #0 = woman, 1 = man
+gender = ['female', 'male', 'nonbinary', 'prefer not to say']
 spokenLanguages = ['English', 'Chinese', 'Spanish', 'Arabic', 'Portuguese', 'Japanese', 'Malaysian', 'Russian', 'French', 'German']
+
+#mentee-specific
 codingLanguages = ['Python', 'JavaScript', 'Java', 'PHP', 'Ruby', 'SQL', 'C#', 'C++', 'iOS', 'other'] #select all that apply
 
