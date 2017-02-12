be5f33983e5fe1064feedb1ca023f2857d6ab02c
Karina Lin <Karina@Karinas-MacBook-Pro.local>
2017-02-11 23:38:23 -0500

    asdf

--- flask/app.py
+++ flask/app.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.secret_key = '\xa7L"\x17>\x9bR:\x92"H\xb6\\k]\x00\xdc4\x85\xa6\xa2\xdd\xa5\x83'
 
+user = {}
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # render the home page
--- match.sql
+++ match.sql
@@ -8,5 +8,7 @@ create table `match` (
 	mentee_email varchar(100),
 	mentor_email varchar(100),
 	INDEX(mentee_email),
-	FOREIGN KEY (mentee_email) REFERENCES accounts (email)
+	INDEX(mentor_email),
+	FOREIGN KEY (mentee_email) REFERENCES accounts (email),
+	FOREIGN KEY (mentor_email) REFERENCES accounts (email)
 	) 
