8b292ad88b4126d151ac91f627a178d58bbb57df
Karina Lin <Karina@Karinas-MacBook-Pro.local>
2017-02-11 19:04:30 -0500

    peppers are spicy

---
+++ mentee_survey.sql
@@ -0,0 +1,26 @@
+-- Mentee Survey Result Table
+
+use whack17;
+
+drop table if exists mentee_survey;
+
+create table mentee_survey (
+
+	email varchar(100),
+	name varchar(50),
+	communication varchar(5),
+	age enum('10-15', '16-20', '21-25', '26-30', '30+'),
+	location char(5),
+	gender enum('female', 'male', 'nonbinary', 'prefer not to say'),
+	spokenLang varchar(10),
+	relationshipGoals enum('casual', 'formal'),
+	mentorType varchar(3),
+
+	occupationMentee enum('middle school student', 'high school student', 'college student', 'working professional', 'other'),
+	interests varchar(8),
+
+	INDEX(email),
+	FOREIGN KEY(email) REFERENCES accounts (email),
+	FOREIGN KEY (email) REFERENCES match (email)
+
+) ENGINE = INNODB;
