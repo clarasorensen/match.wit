c32946c89b521a34c6e658dd2249a2031e9a4dd7
Karina Lin <Karina@Karinas-MacBook-Pro.local>
2017-02-11 23:01:09 -0500

    hi hello

--- flask/wit.py
+++ flask/wit.py
@@ -19,9 +19,26 @@ def connect():
 def createAccount(form):
 	''' creates an account in the db with the survey results based on the form '''
 	curs = connect()
-	email = form['email']
-	
+	type = form['type']
+	row = searchEmail(form['email'])
+	if row == None:
+		curs.execute('INSERT INTO accounts VALUES (%s,%s,%s);', (form['email'],hash_password(form['password']),type))
+		if type=="mentor":
+			curs.execute('INSERT INTO mentor_survey VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);', (form['email'],form['name'],form['communication'],form['age'],form['location'],form['gender'],form['spokenLang'],form['relationshipGoals'],form['mentorType'],form['occupationMentor'],form['codingLang'],form['resume'],form['resume'],form['mentorBio']))
+		if type == "mentee":
+			curs.execute('INSERT INTO mentee_survey VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);', (form['email'],form['name'],form['communication'],form['age'],form['location'],form['gender'],form['spokenLang'],form['relationshipGoals'],form['mentorType'],form['occupationMentee'],form['interests']))
+		return 1
+	else:
+		return 0
 
+
+	
+def searchEmail(email):
+    '''searches if the email is already in db'''
+    curs = connect()
+    curs.execute('SELECT * FROM accounts WHERE email=%s;',(email,))
+    row = curs.fetchall()
+    return row
 
 def hash_password(password):
     '''returns the hashed password with a random salt'''
@@ -31,8 +48,10 @@ def hash_password(password):
     hashedpw = hashlib.sha256(salt.encode()+password.encode()).hexdigest() + ':' + salt
     return hashedpw
    
-
 def check_password(hashedpw,userpw):
     '''checks if the user input password is the same as the hashed password'''
     password, salt = hashedpw.split(':')
-    return password == hashlib.sha256(salt.encode()+userpw.encode()).hexdigest(
+    return password == hashlib.sha256(salt.encode()+userpw.encode()).hexdigest()
+
+
+
